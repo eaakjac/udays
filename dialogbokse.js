dc045f2f5a5b7483d0a3a6c156e5f02ff5af9e92
eaakjac <kasperja1996@gmail.com>
2020-03-20 12:48:35 +0100

    Morten 2

--- dialogbokse.js
+++ dialogbokse.js
@@ -18,8 +18,8 @@ function dialog(hoejde, bredde, baggrundsfarve, fontfarve, placering, indhold, k
     modalindhold.style.fontSize = "16px";
 
     if (placering === "centrer") {
-        modalindhold.style.left = "50%";
-        modalindhold.style.top = "50%";
+        modalindhold.style.left = "30%";
+        modalindhold.style.top = "30%";
         modalindhold.style.transform = "translate(-50%,-50%)";
         modalindhold.style.boxShadow = "5px 5px 5px black";
     } else if (placering === "bund") {
@@ -39,18 +39,6 @@ function dialog(hoejde, bredde, baggrundsfarve, fontfarve, placering, indhold, k
 
 
 // Hovedprogram
-
-document.getElementById("mand2").addEventListener("click", function () {
-    const besked = "<H2>Velkommen indenfor</H2><p><br>Her kan du se nærmere på de fantastiske faciliteter vi har på Sønderhøj 106 - Tryk på pilene for at se nærmere på enten kantinen eller et af vores klasseværelser.</p><p><br>Har du brug for hjælp, så trykker du blot på infoikonet i bunden af siden<br><br></p>";
-
-    dialog("auto", "350px", "white", "black", "centrer", besked, false);
-})
-
-document.getElementById("mandklasselokale").addEventListener("click", function () {
-    const besked = "<H2>Velkommen til vores bygning</H2><p><br>Hej og velkommen til MDU.</p><p><br>Har du brug for hjælp, så trykker du blot på infoikonet i bunden af siden<br><br></p>";
-
-    dialog("auto", "350px", "white", "black", "centrer", besked, false);
-})
 
 
 // JS TIL DIALOGBOKS TIL MAND UDENFOR
